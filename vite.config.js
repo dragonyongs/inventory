4b40b4e6d50d11631a495498b33ab6012b507113
ADY <ujudge@naver.com>
2025-08-07 17:27:01 +0900

    manifestFilename 명시적 작성

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      manifestFilename: 'manifest.json',
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       workbox: {
