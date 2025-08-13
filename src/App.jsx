7ac21de25529dc4fb68e077510e34000ea2f7e16
ADY <ujudge@naver.com>
2025-08-13 10:31:34 +0900

    NotFound 확장자 명시

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import AdminSettings from './pages/admin/AdminSettings'
 import PWAInstallPrompt from './components/PWAInstallPrompt'
 import WorkspaceDashboard from './pages/WorkspaceDashboard'
 import WorkspaceEmptyState from './pages/WorkspaceEmptyState'
-import NotFound from './pages/NotFound'
+import NotFound from './pages/NotFound.jsx'
 import './index.css'
 
 function App() {
