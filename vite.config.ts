c9769c00a599aba7947d326de8518421c8826e71
ADY <ujudge@naver.com>
2025-09-18 14:26:19 +0900

    chore: setup Tailwind CSS v4 with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // 네트워크에서 접근 가능하도록 설정
+    port: 5200,
+  },
+});
