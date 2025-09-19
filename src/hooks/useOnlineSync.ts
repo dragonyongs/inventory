9589080d97729f35be7ff746e10ccf5ebaa4f2d5
ADY <ujudge@naver.com>
2025-09-19 16:10:19 +0900

    백업 파일 및 경로 수정/삭제

--- src/hooks/useOnlineSync.ts
+++ src/hooks/useOnlineSync.ts
@@ -1,7 +1,7 @@
 // src/hooks/useOnlineSync.ts
 import { useEffect } from "react";
 import { useOutboxStore } from "../stores/outboxStore";
-import { useWorkspaceStore } from "../stores/workspaceStore_";
+import { useWorkspaceStore } from "../stores/workspaceStore";
 import { useAuthStore } from "../stores/authStore";
 
 export function useOnlineSync() {
--- src/services/index_.ts
+++
@@ -1,9 +0,0 @@
-import { createNamespacedMemory } from "./db/memoryAdapter.namespaced";
-import { useWorkspaceStore } from "../stores/workspaceStore_";
-
-const ns = createNamespacedMemory();
-
-export function useInventoryService() {
-  const wsId = useWorkspaceStore((s) => s.currentId)!;
-  return ns.service(wsId);
-}
